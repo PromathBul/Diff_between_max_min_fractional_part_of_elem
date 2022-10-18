64ee86d89552a241b9b4da23028967ee83255d65
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-10-18 17:55:35 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,15 @@
+from Methods import Enter_number as enter
+from Methods import Create_list_with_positive_real_nums as real
+from Methods import Fractional_part as fract
+from Methods import Diff_between_max_min as difference
+
+length = enter('Введите количество элементов: ')
+max = enter('Введите максимальное значение элемента: ')
+my_list = real(length, max)
+print(my_list)
+
+my_list = fract(my_list)
+print(my_list)
+
+diff = difference(my_list)
+print(f'Разница между максимальным и минимальным значеним дробной части элементов равна {diff}')
---
+++ Methods.py
@@ -0,0 +1,29 @@
+from random import random
+
+def Enter_number(message):
+    result = int(input(message))
+    return result
+
+def Create_list_with_positive_real_nums(length, max):
+    new_list = []
+    for i in range (length):
+        new_list.append(round(random() * max, 2))
+    return new_list
+
+def Fractional_part (real_number_list):
+    fractional_parts_list = []
+    for item in real_number_list:
+        fractional_parts_list.append(item - int(item))
+        fractional_parts_list[-1] = round(fractional_parts_list[-1], 2)
+    return fractional_parts_list
+
+def Diff_between_max_min (any_list):
+    max = any_list[0]
+    min = any_list[0]
+    for i in range (1, len(any_list)):
+        if any_list[i] > max:
+            max = any_list[i]
+        if any_list[i] < min:
+            min = any_list[i]
+    diff = max - min
+    return diff
